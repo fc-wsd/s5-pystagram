ed492cb8d08d9bc9ae94e738e6957baa5a58421d
Kay Cha <kay@hannal.net>
2016-03-12 15:04:43 +0900

    add exception middleware

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from pystagram.middlewares import HelloWorldError
+
 
 def toppage(request):
-    print(request.just_say)
+    raise HelloWorldError('으악 뭔가 이상해')
     return HttpResponse('hello world')
 
--- pystagram/middlewares.py
+++ pystagram/middlewares.py
@@ -1,7 +1,16 @@
 # pystagram/middlewares.py
+from django.http import HttpResponse
+
+
+class HelloWorldError(Exception):
+    pass
 
 
 class SampleMiddleware(object):
     def process_request(self, request):
         request.just_say = 'Hello world'
+
+    def process_exception(self, request, exc):
+        if isinstance(exc, HelloWorldError):
+            return HttpResponse('어흑, Hello world error...')
 
