8fc3607533273963a99cee2df12a98baac71beab
Kay Cha <kay@hannal.net>
2016-03-26 17:56:48 +0900

    add production settings

--- pystagram/wsgi.py
+++ pystagram/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for pystagram project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pystagram.settings")
+env = os.environ.get('PYSTAGRAM_ENV', '')
+if env:
+    if not env.endswith('_settings'):
+        env = '{}_settings'.format(env)
+else:
+    env = 'settings'
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pystagram.{}".format(env))
 
 application = get_wsgi_application()
